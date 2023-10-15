c12a63100185dcb704bc62599ffbb2d48b4325e3
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2023-10-15 05:55:11 +0500

    setup todo list structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
+const toDoTasks = [
+  {
+    description: 'Buy groceries',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go to the gym',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'Read a book',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Write a report',
+    completed: true,
+    index: 4,
+  },
+];
+
+function showTasks() {
+  const list = document.querySelector('.list');
+  let items = '';
+  toDoTasks.forEach((task) => {
+    items += `<li>
+    <span class="rows"
+      ><input type="checkbox"/><input
+        type="text" value="${task.description}"
+        class="task" /><i class="fa-solid fa-ellipsis-vertical dull"></i
+    ></span>
+  </li>`;
+  });
+  list.innerHTML = items;
 }
-document.body.appendChild(component());
+
+showTasks();
