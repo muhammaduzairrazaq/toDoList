c4ea34b6093efef5121d38d3678f7fba8f8bfd1f
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2023-10-17 14:03:15 +0500

    items add event, icons

--- src/index.js
+++ src/index.js
@@ -25,17 +25,54 @@ const toDoTasks = [
 
 function showTasks() {
   const list = document.querySelector('.list');
-  let items = '';
+  list.innerHTML = '';
   toDoTasks.forEach((task) => {
-    items += `<li>
-    <span class="rows"
-      ><input type="checkbox"/><input
-        type="text" value="${task.description}"
-        class="task" /><i class="fa-solid fa-ellipsis-vertical dull"></i
-    ></span>
-  </li>`;
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <span class="rows">
+        <input type="checkbox"/>
+        <input type="text" value="${task.description}" class="task"/>
+        <i class="fa-solid fa-trash dull"></i>
+        <i class="fa-solid fa-ellipsis-vertical dull"></i>
+      </span>
+    `;
+    list.appendChild(listItem);
+    const checkbox = listItem.querySelector('input[type="checkbox"]');
+    const taskText = listItem.querySelector('.task');
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        taskText.classList.add('completed');
+      } else {
+        taskText.classList.remove('completed');
+      }
+    });
   });
-  list.innerHTML = items;
 }
 
 showTasks();
+
+const taskInput = document.getElementById('taskInput');
+
+function addTask() {
+  const taskDescription = taskInput.value.trim();
+  if (taskDescription === '') {
+    alert('Please enter a task.');
+    return;
+  }
+  const newTask = {
+    description: taskDescription,
+    completed: false,
+    index: toDoTasks.length + 1,
+  };
+
+  toDoTasks.push(newTask);
+
+  taskInput.value = '';
+}
+
+taskInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addTask();
+    showTasks();
+  }
+});
