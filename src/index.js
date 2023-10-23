8bb608b8aa994dc4a5699027ea1e36396667726e
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2023-10-23 11:31:36 +0500

    interactive list completed

--- src/index.js
+++ src/index.js
@@ -1,51 +1,61 @@
 import './style.css';
+import toggleTaskStatus from './status';
 
-const toDoTasks = [
-  {
-    description: 'Buy groceries',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Go to the gym',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Read a book',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Write a report',
-    completed: true,
-    index: 4,
-  },
-];
+let toDoTasks = [];
+
+const storeLocalStorage = () => {
+  const tasksJson = JSON.stringify(toDoTasks);
+  localStorage.setItem('tasks', tasksJson);
+};
+
+const restoreLocalStorage = () => {
+  const tasksJson = localStorage.getItem('tasks');
+  const toDoTasks = JSON.parse(tasksJson);
+  return toDoTasks;
+};
 
 function showTasks() {
   const list = document.querySelector('.list');
   list.innerHTML = '';
-  toDoTasks.forEach((task) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
+  toDoTasks = restoreLocalStorage();
+  toDoTasks.forEach((task, index) => {
+    if (task.completed === false) {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
       <span class="rows">
         <input type="checkbox"/>
         <input type="text" value="${task.description}" class="task"/>
-        <i class="fa-solid fa-trash dull"></i>
-        <i class="fa-solid fa-ellipsis-vertical dull"></i>
+        <i class="fa-solid fa-trash dull trash-icon"></i>
       </span>
     `;
-    list.appendChild(listItem);
-    const checkbox = listItem.querySelector('input[type="checkbox"]');
-    const taskText = listItem.querySelector('.task');
-    checkbox.addEventListener('change', () => {
-      if (checkbox.checked) {
-        taskText.classList.add('completed');
-      } else {
-        taskText.classList.remove('completed');
-      }
-    });
+      list.appendChild(listItem);
+      const checkbox = listItem.querySelector('input[type="checkbox"]');
+      const taskText = listItem.querySelector('.task');
+      const trashIcon = listItem.querySelector('.trash-icon');
+
+      checkbox.addEventListener('change', () => {
+        toggleTaskStatus(toDoTasks, index, taskText, storeLocalStorage, showTasks);
+      });
+
+      trashIcon.addEventListener('click', () => {
+        toDoTasks.splice(index, 1);
+        storeLocalStorage();
+        showTasks();
+      });
+
+      taskText.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+          const taskDescription = taskText.value.trim();
+          alert(taskDescription);
+          if (taskDescription === '') {
+            alert('Please enter a task.');
+          }
+          toDoTasks[index].description = taskDescription;
+          storeLocalStorage();
+          showTasks();
+        }
+      });
+    }
   });
 }
 
@@ -73,6 +83,21 @@ function addTask() {
 taskInput.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     addTask();
+    storeLocalStorage();
     showTasks();
   }
+});
+
+const button = document.querySelector('.clear-btn');
+button.addEventListener('click', () => {
+  toDoTasks = restoreLocalStorage();
+  toDoTasks = toDoTasks.filter((task) => !task.completed);
+  storeLocalStorage();
+  showTasks();
+});
+
+const refresh = document.querySelector('#refresh');
+refresh.addEventListener('click', () => {
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
 });
